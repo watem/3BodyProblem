3fce23d622ee0ccc0c158ab3dc7c921a16ed8c51
watem <watem.main@gmail.com>
2018-05-19 20:46:01 -0400

    Update BodyMaths.java

--- BodyMaths.java
+++ BodyMaths.java
@@ -1,5 +1,5 @@
 /**
-* This logic class contains all of the equations for three body problems involving gravity or electrostatics.
+* This logic class contains all of the equations for three body problems involving gravity.
 * This class also contains the formulae for gravitational Lagrange points.
 * @author Matthew Williams, Yulia Kosharych
 * @version 2018-05-16
@@ -9,7 +9,6 @@ public class BodyMaths {
 
   // import constants
   public static final double G = SolarBody.G;
-  public static final double Ke = Electrostatics.Ke;
 
   /**
   * This method calculates the acceleration that one body causes on another due to gravity
@@ -26,24 +25,6 @@ public class BodyMaths {
     acceleration = -G*massBody*(obj[axis][i]-body[axis][i])/Math.pow(radius(obj, body, i), 3);
     return acceleration;
   }//dualBodyAccel()
-
-  /**
-  * This method calculates the acceleration that one body causes on another due to electrostatic forces
-  * @param obj[][] double               these are the positions of the object that the acceleration is calculated on
-  * @param body[][] double              these are the positions of the body that causes the electrostatic forces on the object
-  * @param qObj double                  this is the charge of the object that is acted apon
-  * @param qBody double                 this is the charge of the other body
-  * @param massObj double               this is the mass of the object whose acceleration is calculated
-  * @param i int                        this is the current timestep
-  * @param axis int                     this is the axis that is currently being calculated (0=x, y=1)
-  * @return acceleration double         this is the value of acceleration along the selected axis
-  * @version 2018-04-30
-  **/
-  public static double electrostaticAccel(double obj[][], double body[][], double qObj, double qBody, double massObj, int i, int axis){
-    double acceleration;
-    acceleration = Ke*qObj*qBody*(obj[axis][i]-body[axis][i])/Math.pow(radius(obj, body, i), 3)/massObj;
-    return acceleration;
-  }//electrostaticAccel()
 
   /**
   * This method calculates the magnitude of a two dimensional vector
